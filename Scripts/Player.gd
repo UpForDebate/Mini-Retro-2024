10cd54935c1b364f03f984e5e2833727d19e550d
Edgarra1619 <edgarr.amorim@gmail.com>
2024-03-03 04:20:02 +0000

    throwing works, now
    gotta sleep

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,6 +8,16 @@ var mainCamera : Camera3D
 var cameraDirection : Node3D
 var interactionRay : RayCast3D
 var interactionUI : Label
+@onready var danglingRope : Node3D =  $"../DanglingRope"
+@onready var throwBobber : RigidBody3D = $"../Bobber"
+
+enum state {
+	rodInHand = 0,
+	fishing = 1,
+	pulling = 2,
+} 
+var gameState : state = state.rodInHand
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -20,13 +30,15 @@ func _ready():
 	interactionUI = $"../UI/INTERACT"
 	cameraDirection = $Node3D
 	
+	
 func _process(delta):
 	
 	mainCamera.global_transform = cameraDirection.global_transform
 	interactionUI.visible = interactionRay.is_colliding()
 	if(Input.is_action_just_pressed("Interact") && interactionRay.is_colliding()):
 		interactionRay.get_collider()._interact()
-	
+	if(Input.is_action_just_pressed("Shoot")):
+		shoot()
 	pass
 	
 	
@@ -53,6 +65,19 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
+	
+	if (gameState==state.pulling):
+		if((global_position - throwBobber.global_position).length() < 1.2):
+			$"../DanglingRope"/RigidBody3D/PinJoint3D/RigidBody3D/PinJoint3D/RigidBody3D/PinJoint3D/Bobber.global_transform = throwBobber.global_transform
+			$"../DanglingRope"/RigidBody3D/PinJoint3D/RigidBody3D/PinJoint3D/RigidBody3D/PinJoint3D/Bobber.linear_velocity = throwBobber.linear_velocity
+			danglingRope.visible = true
+			throwBobber.visible = false
+			gameState = state.rodInHand
+		throwBobber.linear_velocity =  (global_position + Vector3.UP*0.5 - throwBobber.global_position).normalized()*10
+		
+	
+	
+	
 	move_and_slide()
 
 func _input(event):
@@ -62,3 +87,12 @@ func _input(event):
 		mouseMovement.y = clamp(mainCamera.global_rotation.x + mouseMovement.y, deg_to_rad(-70), deg_to_rad(80)) - mainCamera.global_rotation.x;
 		cameraDirection.rotate_x(mouseMovement.y)
 
+func shoot():
+	if(gameState == state.rodInHand):
+		danglingRope.visible = false
+		throwBobber.visible = true
+		throwBobber.position = position + Vector3.UP *0.7 + Vector3.BACK + Vector3.RIGHT
+		throwBobber.linear_velocity = -global_basis.z * 10 + global_basis.y*2;
+		gameState = state.fishing
+		return
+	gameState = state.pulling
